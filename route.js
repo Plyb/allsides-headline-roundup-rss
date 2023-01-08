8ee9b0b5cc93257252c8a453cae2ab07eda4e325
Koby Lewis <kobyjlewis@gmail.com>
2023-01-08 14:49:21 -0700

    add error handling

--- route.js
+++ route.js
@@ -5,8 +5,12 @@ import { getHeadlineRoundups } from "./scraper.js";
 export const app = express();
 
 app.get('/allsides', async (req, res) => {
-    const roundups = await getHeadlineRoundups();
-    const rss = roundupsToRss(roundups);
-    res.set('Content-Type', 'application/rss+xml');
-    res.send(rss);
+    try {
+        const roundups = await getHeadlineRoundups();
+        const rss = roundupsToRss(roundups);
+        res.set('Content-Type', 'application/rss+xml');
+        res.send(rss);
+    } catch (e) {
+        console.error(e);
+    }
 });
